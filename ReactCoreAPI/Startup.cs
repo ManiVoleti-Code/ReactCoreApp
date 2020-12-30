149ed51fddb1c50c566755d8b0249976907d2be7
Mani Srinivas Voleti <mvoleti@deloitte.com>
2020-12-30 21:48:17 +0530

    Completed CRUD operations

--- ReactCoreAPI/Startup.cs
+++ ReactCoreAPI/Startup.cs
@@ -40,10 +40,10 @@ namespace ReactCoreAPI
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddControllers();            
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReactCoreAPI", Version = "v1" });
-            });           
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReactCoreAPI", Version = "v1" });
+            // });           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,8 +52,8 @@ namespace ReactCoreAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReactCoreAPI v1"));
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ReactCoreAPI v1"));
             }
 
             // app.UseHttpsRedirection();
